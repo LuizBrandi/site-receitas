d21b748e85558a2a20b6d509e13f1ca8adbf6119
LuizBrandi <luizfilipebrandi@gmail.com>
2023-10-03 19:17:42 -0300

    setup components

---
+++ src/components/recipeDetails/RecipeDetaisl.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './recipeDetails.module.css'
+
+const RecipeDetaisl = () => {
+  return (
+    <div>RecipeDetaisl</div>
+  )
+}
+
+export default RecipeDetaisl
